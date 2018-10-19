79ab0ff15df5505a41407a3284752ae83bfc592d
Donali <00084417@uca.edu.sv>
2018-10-19 08:37:51 -0600

    Last Commit

--- asset/js/app.js
+++ asset/js/app.js
@@ -27,8 +27,63 @@ navSlide();
 /*********************************Colocar aca el desarrollo de su ejercicio***************************/
 var cont = 1;
 var bitacoras = [];
-var formulario = document.querySelector('#bitacora');
+var formulario = document.getElementById('bitacora');
 
-formulario.addEventListener("click",(e)=>{
+// console.log(formulario[1].value);
+
+formulario.addEventListener("submit",(e)=>{
     e.preventDefault();
+    let bitacora = {
+        cant:cont,
+        fecha:formulario[1].value,
+        descripcion : formulario[2].value,
+        cantidad : formulario[3].value
+    };
+    bitacoras.push(bitacora);
+    cont++;
+    mostrar();
 });
+const crearElemento = (bitacora,tbody) =>{
+    let tr = document.createElement('tr');
+    Object.values(bitacora).forEach(item=>{
+       let td =  document.createElement('td');
+       let content = document.createTextNode(item);
+       td.appendChild(content);
+       tr.append(td);
+       tr.setAttribute('class','click');
+
+    });
+    tbody.appendChild(tr);
+
+};    
+
+const eliminar = (tbody)=>{
+    while(tbody.firstChild){
+        tbody.removeChild(tbody.firstChild);
+    }
+}
+const agregar= ()=>{ 
+
+    var eventtr = document.querySelectorAll(".click");
+    eventtr.forEach((item, index) => { 
+        console.log(item.childNodes[0].textContent);
+        item.addEventListener("click", () => {
+        
+        document.querySelector("#fecha").value = item.childNodes[0].textContent; 
+        document.querySelector("#descp").value = item.childNodes[1].textContent; 
+        document.querySelector("#cant").value = item.childNodes[2].textContent; 
+        }); 
+    });
+    console.log(eventtr);             
+
+} 
+const mostrar = ()=>{ 
+    if (document.querySelector(".tabla-btc tbody").childElementCount > 0) { 
+        
+        eliminar(document.querySelector(".tabla-btc tbody")); 
+    } 
+    bitacoras.forEach(item => { 
+     crearElemento(item, document.querySelector(".tabla-btc tbody")); 
+    }); 
+    agregar(); 
+} 
